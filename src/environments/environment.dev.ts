b8f0c2914b27fbc5e48528f0c500bea57123d21b
jrd281 <justindallas@gmail.com>
2021-12-23 14:21:17 -0500

    Committing after setting up the project.  I'm going to branch and tear out what I don't neeed

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    stage: 'staging',
+    production: false,
+    resourceServerUrl: ''
+};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    stage: 'test',
+    production: false,
+    resourceServerUrl: ''
+};

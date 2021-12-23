5061bac870d41d1f7d02c1863e39f9987d6888a5
jrd281 <justindallas@gmail.com>
2021-12-23 14:47:55 -0500

    About a quarter of the way through with pulling out the nunnecessary stuff

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -29,13 +29,6 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 type : 'basic',
                 icon : 'heroicons_outline:cash',
                 link : '/dashboards/finance'
-            },
-            {
-                id   : 'dashboards.crypto',
-                title: 'Crypto',
-                type : 'basic',
-                icon : 'heroicons_outline:currency-dollar',
-                link : '/dashboards/crypto'
             }
         ]
     },
@@ -47,46 +40,11 @@ export const defaultNavigation: FuseNavigationItem[] = [
         icon    : 'heroicons_outline:home',
         children: [
             {
-                id   : 'apps.academy',
-                title: 'Academy',
-                type : 'basic',
-                icon : 'heroicons_outline:academic-cap',
-                link : '/apps/academy'
-            },
-            {
-                id   : 'apps.chat',
-                title: 'Chat',
-                type : 'basic',
-                icon : 'heroicons_outline:chat-alt',
-                link : '/apps/chat'
-            },
-            {
                 id   : 'apps.contacts',
                 title: 'Contacts',
                 type : 'basic',
                 icon : 'heroicons_outline:user-group',
                 link : '/apps/contacts'
-            },
-            {
-                id      : 'apps.ecommerce',
-                title   : 'ECommerce',
-                type    : 'collapsable',
-                icon    : 'heroicons_outline:shopping-cart',
-                children: [
-                    {
-                        id   : 'apps.ecommerce.inventory',
-                        title: 'Inventory',
-                        type : 'basic',
-                        link : '/apps/ecommerce/inventory'
-                    }
-                ]
-            },
-            {
-                id   : 'apps.file-manager',
-                title: 'File Manager',
-                type : 'basic',
-                icon : 'heroicons_outline:cloud',
-                link : '/apps/file-manager'
             },
             {
                 id      : 'apps.help-center',
@@ -122,38 +80,6 @@ export const defaultNavigation: FuseNavigationItem[] = [
                     }
                 ]
             },
-            {
-                id   : 'apps.mailbox',
-                title: 'Mailbox',
-                type : 'basic',
-                icon : 'heroicons_outline:mail',
-                link : '/apps/mailbox',
-                badge: {
-                    title  : '27',
-                    classes: 'px-2 bg-pink-600 text-white rounded-full'
-                }
-            },
-            {
-                id   : 'apps.notes',
-                title: 'Notes',
-                type : 'basic',
-                icon : 'heroicons_outline:pencil-alt',
-                link : '/apps/notes'
-            },
-            {
-                id   : 'apps.scrumboard',
-                title: 'Scrumboard',
-                type : 'basic',
-                icon : 'heroicons_outline:view-boards',
-                link : '/apps/scrumboard'
-            },
-            {
-                id   : 'apps.tasks',
-                title: 'Tasks',
-                type : 'basic',
-                icon : 'heroicons_outline:check-circle',
-                link : '/apps/tasks'
-            }
         ]
     },
     {
